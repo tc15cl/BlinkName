33d8d897ed5c6a0ce7844881dae3d13c220eb297
TIM COOK <tjcook@deakin.edu.au>
2020-03-24 14:49:32 +1100

    Updated comments

--- src/BlinkName.cpp
+++ src/BlinkName.cpp
@@ -15,23 +15,19 @@
 // Morse code LED blink - D7
 // -----------------------------------
 
+
 //led1 is D7 - logical identifier 
-
-
 void setup();
 void loop();
-#line 15 "d:/GDRIVE/School/DEVIT_UNI/2020_T1/SIT210/OnTrack/SIT210-02.1P/BlinkName/src/BlinkName.ino"
+#line 14 "d:/GDRIVE/School/DEVIT_UNI/2020_T1/SIT210/OnTrack/SIT210-02.1P/BlinkName/src/BlinkName.ino"
 int led1 = D7;
 
-// Last time, we only needed to declare pins in the setup function.
-// This time, we are also going to register our Particle function
 
 void setup()
 {
 
    //Configuring led1 to be an output
    pinMode(led1, OUTPUT);
-
 
    //Initialise led1 to be off
    digitalWrite(led1, LOW);
@@ -40,41 +36,40 @@ void setup()
 
 
 //Loop through TIM in morse code DASH-DOT-DOT-DASH(- .. -)
-//DASH = 3000ms
+//DASH = 2000ms
 //DOT = 1000ms
-
 void loop()
 {
-	//Switch led1 on for 3 seconds
+	//Switch led1 on for 2 seconds (DASH)
 	digitalWrite(led1, HIGH);
 	delay(2000);
 
-	//Switch led1 off for 1 second
+	//Switch led1 off for half second
 	digitalWrite(led1, LOW);
-  delay(500);
+  	delay(500);
 
-  	//Switch led1 on for 1 seconds
+  	//Switch led1 on for 1 second (DOT)
 	digitalWrite(led1, HIGH);
 	delay(1000);
 
-	//Switch led1 off for 1 second
+	//Switch led1 off for half second
 	digitalWrite(led1, LOW);
-  delay(500);
+  	delay(500);
 
-    	//Switch led1 on for 1 seconds
+    	//Switch led1 on for 1 second (DOT)
 	digitalWrite(led1, HIGH);
 	delay(1000);
 
-	//Switch led1 off for 1 second
+	//Switch led1 off for half second
 	digitalWrite(led1, LOW);
-  delay(500);
+  	delay(500);
 
-  	//Switch led1 on for 3 seconds
+  	//Switch led1 on for 2 seconds (DASH)
 	digitalWrite(led1, HIGH);
 	delay(2000);
 
 	// Wait 4 seconds before repeating sequence.
-  digitalWrite(led1, LOW);
+  	digitalWrite(led1, LOW);
 	delay(4000);
 
 }
--- src/BlinkName.ino
+++ src/BlinkName.ino
@@ -9,20 +9,16 @@
 // Morse code LED blink - D7
 // -----------------------------------
 
+
 //led1 is D7 - logical identifier 
-
-
 int led1 = D7;
 
-// Last time, we only needed to declare pins in the setup function.
-// This time, we are also going to register our Particle function
 
 void setup()
 {
 
    //Configuring led1 to be an output
    pinMode(led1, OUTPUT);
-
 
    //Initialise led1 to be off
    digitalWrite(led1, LOW);
@@ -33,7 +29,6 @@ void setup()
 //Loop through TIM in morse code DASH-DOT-DOT-DASH(- .. -)
 //DASH = 2000ms
 //DOT = 1000ms
-
 void loop()
 {
 	//Switch led1 on for 2 seconds (DASH)
@@ -42,7 +37,7 @@ void loop()
 
 	//Switch led1 off for half second
 	digitalWrite(led1, LOW);
-  delay(500);
+  	delay(500);
 
   	//Switch led1 on for 1 second (DOT)
 	digitalWrite(led1, HIGH);
@@ -50,7 +45,7 @@ void loop()
 
 	//Switch led1 off for half second
 	digitalWrite(led1, LOW);
-  delay(500);
+  	delay(500);
 
     	//Switch led1 on for 1 second (DOT)
 	digitalWrite(led1, HIGH);
@@ -58,14 +53,14 @@ void loop()
 
 	//Switch led1 off for half second
 	digitalWrite(led1, LOW);
-  delay(500);
+  	delay(500);
 
   	//Switch led1 on for 2 seconds (DASH)
 	digitalWrite(led1, HIGH);
 	delay(2000);
 
 	// Wait 4 seconds before repeating sequence.
-  digitalWrite(led1, LOW);
+  	digitalWrite(led1, LOW);
 	delay(4000);
 
 }
